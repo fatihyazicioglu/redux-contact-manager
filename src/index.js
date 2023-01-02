b0bb452dfbea890d1afd36fe844a705ab56a0bd4
fatihyazicioglu <fy134388@gmail.com>
2023-01-02 17:18:40 +0100

    redux toolkits

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import PeopleList from './components/PeopleList';
 import AddPersonForm from './components/AddPersonForm';
-import  {createStore}  from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import store from './app/store';
+import './index.css';
+
+const initialState = {
+  contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"] 
+  };
+
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_PERSON':
+      return {...state,
+        contacts: [...state.contacts, action.data]}
+    default:
+      return state;
+  }
+}
 
 const store = createStore(reducer);
 ReactDOM.render(
